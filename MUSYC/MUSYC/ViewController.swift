c50965a8a3a4083768b3de186e3bdffd47d1f184
Anh Le <a.v.le@wustl.edu>
2020-11-16 11:00:57 +0700

    Initial Commit

---
+++ MUSYC/MUSYC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MUSYC
+//
+//  Created by Anh Le on 11/16/20.
+//  Copyright © 2020 Anh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
